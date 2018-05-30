33f4e4212d820bd7b767e3908b1ddae101cb674c
Sergio Vega <svegaxmr@gmail.com>
2018-05-30 02:37:21 -0500

    Clean up method + hex to string

--- monero-utils-core/src/com/svega/moneroutils/BinHexUtils.kt
+++ monero-utils-core/src/com/svega/moneroutils/BinHexUtils.kt
@@ -19,11 +19,24 @@ class BinHexUtils {
                 throw MoneroException("Hex string has invalid length!")
             if(hex.isEmpty())
                 return byteArrayOf()
-            val res = ByteArray(hex.length / 2, {_ -> 0})
-            for (i in 0 until hex.length / 2) {
-                res[i] = Integer.parseInt(hex.replace('\u0000','0').substring(i * 2, i * 2 + 2), 16).toByte()
+            val l = hex.length
+            val data = ByteArray(l / 2)
+            var i = 0
+            while (i < l) {
+                data[i / 2] = ((Character.digit(hex[i], 16) shl 4) + Character.digit(hex[i + 1], 16)).toByte()
+                i += 2
             }
-            return res
+            return data
+        }
+
+        fun convertHexToString(hex: String): String {
+            val sb = StringBuilder()
+            var i = 0
+            while (i < hex.length - 1) {
+                sb.append(Integer.parseInt(hex.substring(i, i + 2), 16).toChar())
+                i += 2
+            }
+            return sb.toString()
         }
 
         fun binaryToHex(bin: Array<UInt8>) : String{
