b0f6aea822eab76580c0228bcc98edaea3cbfe01
Sergio Vega <svegaxmr@gmail.com>
2018-06-12 17:33:46 -0500

    Java 10 + JAR Build

---
+++ monero-utils-core/src/module-info.java
@@ -0,0 +1,6 @@
+module monero.utils.core {
+    exports com.svega.moneroutils.addresses;
+    exports com.svega.moneroutils.crypto;
+    exports com.svega.moneroutils.transactions;
+    exports com.svega.moneroutils;
+}
