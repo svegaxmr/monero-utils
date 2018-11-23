6e1b05df38d102389da263931a107614d9311196
Nicholas Johnson <sylvyrfysh@gmail.com>
2018-11-22 23:55:13 -0600

    Add block hashing base

--- src/main/java/com/svega/moneroutils/blocks/Block.kt
+++ src/main/java/com/svega/moneroutils/blocks/Block.kt
@@ -1,153 +1,147 @@
 package com.svega.moneroutils.blocks
 
 import com.svega.moneroutils.BinHexUtils
-import com.svega.moneroutils.transactions.CoinbaseTransaction
+import com.svega.moneroutils.crypto.Hashing
+import com.svega.moneroutils.crypto.MoneroSerializable
+import com.svega.moneroutils.crypto.slowhash.SlowHash
+import com.svega.moneroutils.crypto.slowhash.SlowHashResult
+import com.svega.moneroutils.getVarInt
+import com.svega.moneroutils.transactions.Transaction
+import com.svega.moneroutils.varIntData
+import com.svega.moneroutils.writeVarInt
+import java.io.ByteArrayOutputStream
+import java.io.DataOutputStream
 import java.nio.ByteBuffer
 import java.util.*
 import kotlin.collections.ArrayList
 
-open class Block private constructor() {
+
+open class Block: MoneroSerializable{
     lateinit var blockHeader: BlockHeader
-    lateinit var coinbase: CoinbaseTransaction
+    lateinit var coinbase: Transaction
     lateinit var byteBuffer: ByteBuffer
-    var extra = ByteArray(0)
-        private set
-    var unknown = Long.MIN_VALUE
-        private set
     val txids = ArrayList<ByteArray>()
-        get() = ArrayList(field)
     var numRCTSigs = -1
+    val hasRCTSigs get() = blockHeader.major >= 4
+    var isHashValid = false
+    var hash =  ByteArray(0)
         private set
-    val hasRCTSigs get() = blockHeader.major > 1
+        get() {
+            if(isHashValid)
+                return field
+            else{
+                val res = ByteArray(32)
+                isHashValid = getBlockHash(this, res)
+                field = res
+            }
+            return if(isHashValid) field else ByteArray(0)
+        }
+    override fun toBlob(): ByteArray {
+        val b = ByteArrayOutputStream()
+        val d = DataOutputStream(b)
+        d.write(blockHeader.toBlob())
+        d.write(coinbase.toBlob())
+        d.writeVarInt(txids.size)
+        for(i in txids){
+            d.write(i)
+        }
+        if(hasRCTSigs)
+            d.writeVarInt(numRCTSigs)
+        return b.toByteArray()
+    }
+    fun getPOWHash(nonce: Long = blockHeader.nonce): SlowHashResult {
+        blockHeader.nonce = nonce
+        val toHash = getBlockHashingBlob(this)
+        val res = UByteArray(32)
+        SlowHash.cnSlowHash(toHash.asUByteArray(), res, hashVer(blockHeader.major))
+        return SlowHashResult(this, nonce, res)
+    }
     companion object {
-        fun parseBlobHeader(header: String): Block {
+        val hashVer = { i: Int -> when(i){
+                    1,2,3,4,5,6 -> 0
+                    7 -> 1
+                    8,9 -> 2
+                    else -> throw RuntimeException("Block version $i is not supported!")
+                }
+            }
+        fun parseBlobHeader(header: ByteArray): Block {
             val block = Block()
             with(block){
-                byteBuffer = ByteBuffer.allocateDirect(header.length / 2).put(BinHexUtils.hexToByteArray(header)).rewind()
+                byteBuffer = ByteBuffer.wrap(header).rewind()
 
                 blockHeader = BlockHeader.parseBlockBlobHeader(byteBuffer)
-                coinbase = CoinbaseTransaction.parseFromBlob(byteBuffer)
+                coinbase = Transaction.parseBlobHeader(byteBuffer)
 
-                if(hasRCTSigs) {
-                    numRCTSigs = byteBuffer.getVarInt()
-                }
-                unknown = byteBuffer.getVarLong()
                 val numTxes = byteBuffer.getVarInt()
                 val txid = ByteArray(32)
                 for(i in 0 until numTxes){
                     byteBuffer.get(txid)
                     txids.add(Arrays.copyOf(txid, 32))
                 }
-                val extraSize = byteBuffer.getVarInt()
-                extra = ByteArray(extraSize)
-                byteBuffer.get(extra)
+
+                if(hasRCTSigs and (byteBuffer.remaining() > 0)) {
+                    numRCTSigs = byteBuffer.getVarInt()
+                }
             }
             return block
+        }
+
+        var blockHashesCalculated = 0
+        private fun getBlockHash(b: Block, res: ByteArray): Boolean {
+            ++blockHashesCalculated
+            val ret = calculateBlockHash(b, res)
+            if(!ret)
+                return false
+            return true
+        }
+
+        fun getBlockHashingBlob(b: Block): ByteArray {
+            val hBlob = b.blockHeader.toBlob()
+            val trHash = getTXTreeHash(b)
+            return hBlob + trHash + (b.txids.size + 1).varIntData()
+        }
+
+        fun getTXTreeHash(b: Block): ByteArray {
+            val txIDs = Array(1 + b.txids.size) {ByteArray(32)}
+            var place = 0
+            val h = ByteArray(32)
+            Transaction.getTXHash(b.coinbase, h)
+            txIDs[place++] = h
+            for(t in b.txids)
+                txIDs[place++] = t
+            return getTXTreeHash(txIDs)
+        }
+
+        fun getTXTreeHash(b: Array<ByteArray>): ByteArray {
+            val ret = ByteArray(32)
+            Hashing.treeHash(b, ret)
+            return ret
+        }
+
+        private val correct202612 = BinHexUtils.hexToByteArray("3a8a2b3a29b50fc86ff73dd087ea43c6f0d6b8f936c849194d5c84c737903966")
+        private val existing202612 = BinHexUtils.hexToByteArray("bbd604d2ba11ba27935e006ed39c9bfdd99b76bf4a50654bc1e1e61217962698")
+        private fun calculateBlockHash(b: Block, res: ByteArray): Boolean{
+            val blobHash = Hashing.getBlobHash(b.toBlob())
+
+            if (blobHash.contentEquals(existing202612)){
+                System.arraycopy(correct202612, 0, res, 0, 32)
+                return true
+            }
+
+            val result = Hashing.getObjectHash(getBlockHashingBlob(b), res)
+
+            if(result){
+                if(res.contentEquals(existing202612)){
+                    System.arraycopy(ByteArray(32), 0, res, 0, 32)
+                    return false
+                }
+            }
+
+            return result
         }
     }
 
     override fun toString(): String {
-        return "Block(\n\tblockHeader=$blockHeader,\n\tcoinbase=$coinbase,\n\textra=${BinHexUtils.binaryToHex(extra)}, unknown=$unknown, txids=$txids)"
+        return "Block(\n\tblockHeader=$blockHeader,\n\tcoinbase=$coinbase, txids=$txids)"
     }
-}
-
-fun ByteBuffer.getVarLong(): Long {
-    var tmp = this.get().toLong()
-    if (tmp >= 0) {
-        return tmp
-    }
-    var result = tmp and 0x7f
-    tmp = this.get().toLong()
-    if (tmp >= 0) {
-        result = result or (tmp shl 7)
-    } else {
-        result = result or (tmp and 0x7f shl 7)
-        tmp = this.get().toLong()
-        if (tmp >= 0) {
-            result = result or (tmp shl 14)
-        } else {
-            result = result or (tmp and 0x7f shl 14)
-            tmp = this.get().toLong()
-            if (tmp >= 0) {
-                result = result or (tmp shl 21)
-            } else {
-                result = result or (tmp and 0x7f shl 21)
-                tmp = this.get().toLong()
-                if (tmp >= 0) {
-                    result = result or (tmp shl 28)
-                } else {
-                    result = result or (tmp and 0x7f shl 28)
-                    tmp = this.get().toLong()
-                    if (tmp >= 0) {
-                        result = result or (tmp shl 35)
-                    } else {
-                        result = result or (tmp and 0x7f shl 35)
-                        tmp = this.get().toLong()
-                        if (tmp >= 0) {
-                            result = result or (tmp shl 42)
-                        } else {
-                            result = result or (tmp and 0x7f shl 42)
-                            tmp = this.get().toLong()
-                            if (tmp >= 0) {
-                                result = result or (tmp shl 49)
-                            } else {
-                                result = result or (tmp and 0x7f shl 49)
-                                tmp = this.get().toLong()
-                                if (tmp >= 0) {
-                                    result = result or (tmp shl 56)
-                                } else {
-                                    result = result or (tmp and 0x7f shl 56)
-                                    result = result or (this.get().toLong() shl 63)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return result
-}
-
-fun ByteBuffer.getVarInt(): Int {
-    var tmp: Int = this.get().toInt()
-    if ((tmp) >= 0) {
-        return tmp
-    }
-    var result = tmp and 0x7f
-    tmp = this.get().toInt()
-    if (tmp >= 0) {
-        result = result or (tmp shl 7)
-    } else {
-        result = result or (tmp and 0x7f shl 7)
-        tmp = this.get().toInt()
-        if (tmp >= 0) {
-            result = result or (tmp shl 14)
-        } else {
-            result = result or (tmp and 0x7f shl 14)
-            tmp = this.get().toInt()
-            if ((tmp) >= 0) {
-                result = result or (tmp shl 21)
-            } else {
-                result = result or (tmp and 0x7f shl 21)
-                tmp = this.get().toInt()
-                result = result or (tmp shl 28)
-                while (tmp < 0) {
-                    // We get into this loop only in the case of overflow.
-                    // By doing this, we can call getVarInt() instead of
-                    // getVarLong() when we only need an int.
-                    tmp = this.get().toInt()
-                }
-            }
-        }
-    }
-    return result
-}
-
-fun ByteBuffer.printNextBytes(n: Int){
-    mark()
-    val one = if(n < remaining()) ByteArray(n) else ByteArray(remaining())
-    get(one)
-    reset()
-    println("next is ${BinHexUtils.binaryToHex(one)}")
 }
