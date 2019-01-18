32b9db33f9090b03fe7f9d6578233ae873db954e
Sergio Vega <svegaxmr@gmail.com>
2019-01-18 17:13:18 -0600

    Add static ADDRESS_{LENGTH|BYTE} values

--- src/main/java/com/svega/moneroutils/addresses/IntegratedAddress.kt
+++ src/main/java/com/svega/moneroutils/addresses/IntegratedAddress.kt
@@ -2,15 +2,16 @@ package com.svega.moneroutils.addresses
 
 import com.svega.moneroutils.*
 import com.svega.moneroutils.crypto.slowhash.Keccak
+import com.svega.moneroutils.exceptions.MoneroException
 
 @ExperimentalUnsignedTypes
 class IntegratedAddress : MoneroAddress {
-    override val LENGTH = 106
-    override val BYTES = 77
+    override val LENGTH = ADDRESS_LENGTH
+    override val BYTES = ADDRESS_BYTES
     var paymentID: ByteArray = ByteArray(8)
-        set(value){
-            if(value.size != 8)
-                throw MoneroException("Value is size ${value.size}, needs to be 8")
+        set(value) {
+            if (value.size != 8)
+                throw MoneroException("Payment ID is size ${value.size}, needs to be 8")
             field = value
         }
     var paymentIDStr: String
@@ -23,31 +24,36 @@ class IntegratedAddress : MoneroAddress {
 
     constructor(address: String, net: NetType) : super(address, net) {
         paymentID = bytes.asByteArray().sliceArray(IntRange(65, 72))
-        val mAddrKeys = UByteArray(1) {net.INTEGRATED} concat bytes.copyOfRange(1, 65)
+        val mAddrKeys = UByteArray(1) { net.INTEGRATED } concat bytes.copyOfRange(1, 65)
         val mAddrCSum = Keccak.fullChecksum(mAddrKeys)
         val mAddrBytes = mAddrKeys concat mAddrCSum
-        validateChecksum(mAddrBytes,"Integrated address splitting")
-        mainAddressStr = Base58.encode(BinHexUtils.binaryToHex(mAddrBytes))
+        validateChecksum(mAddrBytes, "Integrated address splitting")
+        mainAddressStr = Base58.encode(BinHexUtils.ubinaryToHex(mAddrBytes))
         validate()
     }
 
-    constructor(mainAddress: MainAddress, paymentIDStr: String) : super(mainAddress.address, mainAddress.net){
+    constructor(mainAddress: MainAddress, paymentIDStr: String) : super(mainAddress.address, mainAddress.netType) {
         this.paymentIDStr = paymentIDStr
         this.mainAddressStr = mainAddress.address
         val mAddrKeys = Base58.decode(mainAddressStr).copyOfRange(1, 65)
-        val toHash = UByteArray(1) {mainAddress.net.INTEGRATED} concat mAddrKeys concat paymentID.asUByteArray()
+        val toHash = UByteArray(1) { mainAddress.netType.INTEGRATED } concat mAddrKeys concat paymentID.asUByteArray()
         val checksum = Keccak.fullChecksum(toHash)
         bytes = toHash concat checksum
         MoneroAddress.validateChecksum(bytes, "Integrated address creation")
-        address = Base58.encode(BinHexUtils.binaryToHex(bytes))
+        address = Base58.encode(BinHexUtils.ubinaryToHex(bytes))
         validate()
     }
 
-    override fun validate(){
+    override fun validate() {
         super.validate()
-        if(paymentID.size == 32)
+        if (paymentID.size == 32)
             throw MoneroException("Legacy payment ID's are not supported!")
-        if(paymentID.size != 8)
+        if (paymentID.size != 8)
             throw MoneroException("Payment ID's must be 8 bytes long!")
+    }
+
+    companion object {
+        const val ADDRESS_LENGTH = 106
+        const val ADDRESS_BYTES = 77
     }
 }
--- src/main/java/com/svega/moneroutils/addresses/SubAddress.kt
+++ src/main/java/com/svega/moneroutils/addresses/SubAddress.kt
@@ -4,13 +4,19 @@ import com.svega.moneroutils.AddressType
 import com.svega.moneroutils.NetType
 
 @ExperimentalUnsignedTypes
-class SubAddress: MoneroAddress{
-    constructor(address: String, netType: NetType): super(address, netType)
+class SubAddress : MoneroAddress {
+    constructor(address: String, netType: NetType) : super(address, netType)
     constructor(key: FullKey, net: NetType) : super(key, net, AddressType.SUBADDRESS)
 
-    override val LENGTH = 95
-    override val BYTES = 69
-    init{
+    override val LENGTH = ADDRESS_LENGTH
+    override val BYTES = ADDRESS_BYTES
+
+    init {
         validate()
+    }
+
+    companion object {
+        const val ADDRESS_LENGTH = 95
+        const val ADDRESS_BYTES = 69
     }
 }
